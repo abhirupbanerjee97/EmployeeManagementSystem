c9c32fbbaaf49d6784753b4cf757e188d63470fd
abhirupbanerjee97 <43820709+abhirupbanerjee97@users.noreply.github.com>
2018-10-08 16:44:41 +0530

    Employee Management System

---
+++ EmployeeAdministrationPortalSoftwareProject/src/AppSession.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ABHIRUP
+ */
+public class AppSession {
+    public static String emp_id="";
+    public static String name="";
+    public static String adm_id="";
+}
